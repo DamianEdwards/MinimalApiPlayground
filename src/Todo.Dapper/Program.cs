bb01ca58eb04ab88feac385615528c4ccac52d08
DamianEdwards <damian@damianedwards.com>
2021-07-12 12:46:57 -0700

    Todo.EFCore added & Dapper updates
    - Todo.EFCore added that matches Todo.Dapper
    - Added DB initialization in Todo.Dapper

--- src/Todo.Dapper/Program.cs
+++ src/Todo.Dapper/Program.cs
@@ -1,6 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-using Microsoft.Data.Sqlite;
 using Dapper;
+using Microsoft.Data.Sqlite;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +8,16 @@ builder.Services.AddScoped<SqliteConnection>(_ => new SqliteConnection("Data Sou
 
 var app = builder.Build();
 
+using IServiceScope scope = app.Services.CreateScope();
+await EnsureDb(scope.ServiceProvider.GetRequiredService<SqliteConnection>());
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => new { Hello = "World" });
 
 app.MapGet("/todos", async (SqliteConnection db) =>
     await db.QueryAsync<Todo>("SELECT * FROM Todos"));
@@ -32,48 +36,54 @@ app.MapGet("/todos/{id}", async (int id, SqliteConnection db) =>
 app.MapPost("/todos", async (Todo todo, SqliteConnection db) =>
 {
     if (!MinimalValidation.TryValidate(todo, out var errors))
-        return Results.BadRequest(errors);
+        return Results.ValidationProblem(errors);
 
-    var newTodo = await db.QuerySingleAsync<Todo>("INSERT INTO Todos(Title, IsComplete) Values(@Title, @IsComplete) RETURNING *", todo);
+    var newTodo = await db.QuerySingleAsync<Todo>(
+        "INSERT INTO Todos(Title, IsComplete) Values(@Title, @IsComplete) RETURNING * ", todo);
 
     return Results.Created($"/todos/{newTodo.Id}", newTodo);
 });
 
-app.MapPut("/todos/{id}", async (int id, Todo inputTodo, SqliteConnection db) =>
+app.MapPut("/todos/{id}", async (int id, Todo todo, SqliteConnection db) =>
 {
-    if (!MinimalValidation.TryValidate(inputTodo, out var errors))
-        return Results.BadRequest(errors);
+    todo.Id = id;
+    if (!MinimalValidation.TryValidate(todo, out var errors))
+        return Results.ValidationProblem(errors);
 
-    inputTodo.Id = id;
-    var rowCount = await db.ExecuteAsync("UPDATE Todos SET Title = @Title, IsComplete = @IsComplete WHERE Id = @Id", inputTodo);
-
-    if (rowCount == 0) return
-        Results.NotFound();
-
-    return Results.NoContent();
+    return await db.ExecuteAsync("UPDATE Todos SET Title = @Title, IsComplete = @IsComplete WHERE Id = @Id", todo) == 1
+        ? Results.NoContent()
+        : Results.NotFound();
 });
+
+app.MapPut("/todos/{id}/mark-complete", async (int id, SqliteConnection db) =>
+    await db.ExecuteAsync("UPDATE Todos SET IsComplete = true WHERE Id = @Id", new { id }) == 1
+        ? Results.NoContent()
+        : Results.NotFound());
+
+app.MapPut("/todos/{id}/mark-incomplete", async (int id, SqliteConnection db) =>
+    await db.ExecuteAsync("UPDATE Todos SET IsComplete = false WHERE Id = @Id", new { id }) == 1
+        ? Results.NoContent()
+        : Results.NotFound());
 
 app.MapDelete("/todos/{id}", async (int id, SqliteConnection db) =>
-{
-    var rowCount = await db.ExecuteAsync("DELETE FROM Todos WHERE Id = @id", new { id });
-
-    if (rowCount == 0) return
-        Results.NotFound();
-
-    return Results.Ok();
-});
+    await db.ExecuteAsync("DELETE FROM Todos WHERE Id = @id", new { id }) == 1
+        ? Results.NoContent()
+        : Results.NotFound());
 
 app.MapDelete("/todos/delete-all", async (SqliteConnection db) =>
-{
-    var rowCount = await db.ExecuteAsync("DELETE FROM Todos");
-
-    if (rowCount == 0) return
-        Results.NotFound();
-
-    return Results.Ok();
-});
+    Results.Ok(await db.ExecuteAsync("DELETE FROM Todos")));
 
 app.Run();
+
+Task EnsureDb(SqliteConnection db)
+{
+    var sql = $@"CREATE TABLE IF NOT EXISTS Todos (
+                  {nameof(Todo.Id)} INTEGER PRIMARY KEY AUTOINCREMENT,
+                  {nameof(Todo.Title)} TEXT NOT NULL,
+                  {nameof(Todo.IsComplete)} BOOL DEFAULT FALSE
+                );";
+    return db.ExecuteAsync(sql);
+}
 
 class Todo
 {
---
+++ src/Todo.EFCore/Program.cs
@@ -0,0 +1,123 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDb>(options => options.UseSqlite("Data Source=todos.db"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+using IServiceScope scope = app.Services.CreateScope();
+await scope.ServiceProvider.GetRequiredService<TodoDb>().Database.EnsureCreatedAsync();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => new { Hello = "World" });
+
+app.MapGet("/todos", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapGet("/todos/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todos/incomplete", async (TodoDb db) =>
+    await db.Todos.Where(t => !t.IsComplete).ToListAsync());
+
+app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id) is Todo todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.MapPost("/todos", async (Todo todo, TodoDb db) =>
+{
+    if (!MinimalValidation.TryValidate(todo, out var errors))
+        return Results.ValidationProblem(errors);
+
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todos/{todo.Id}", todo);
+});
+
+app.MapPut("/todos/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    if (!MinimalValidation.TryValidate(inputTodo, out var errors))
+        return Results.ValidationProblem(errors);
+
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Title = inputTodo.Title;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapPut("/todos/{id}/mark-complete", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        todo.IsComplete = true;
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapPut("/todos/{id}/mark-incomplete", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        todo.IsComplete = false;
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapDelete("/todos/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/todos/delete-all", async (TodoDb db) =>
+    Results.Ok(await db.Database.ExecuteSqlRawAsync("DELETE FROM Todos")));
+
+app.Run();
+
+class Todo
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Title { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
