8d58296d74bbe6b8cc63a9d7815b9e6977222dfb
DamianEdwards <damian@damianedwards.com>
2021-08-11 13:00:29 -0700

    Remove unused Required attribute

--- src/Todo.Dapper/Program.cs
+++ src/Todo.Dapper/Program.cs
@@ -127,7 +127,6 @@ async Task EnsureDb(string connectionString, ILogger logger)
 class Todo
 {
     public int Id { get; set; }
-    [Required]
     public string? Title { get; set; }
     public bool IsComplete { get; set; }
 }
