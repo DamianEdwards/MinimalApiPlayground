7e0aeb100ed0c73911d4c514ef51b85bb43f5a31
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-08-16 13:12:50 -0400

    SmokeTests is missing a namespace
    I tried this with the latest .NET 6 RC1 nightly
    build, so I didn't see any implicit namespace
    that would fix this.

--- tests/Todo.Dapper.Tests/SmokeTests.cs
+++ tests/Todo.Dapper.Tests/SmokeTests.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
