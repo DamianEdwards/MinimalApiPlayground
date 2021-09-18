4dceef73de99984cd679a9011d538931ec17c066
David Fowler <davidfowl@gmail.com>
2021-09-17 20:19:03 -0700

    Added type converter support via TypeConverter<T>

--- src/MinimalApiPlayground/Program.cs
+++ src/MinimalApiPlayground/Program.cs
@@ -148,6 +148,12 @@ app.MapGet("/paged2", (ModelBinder<PagedData> paging) =>
     $"model: {paging.Model}, valid: {paging.ModelState.IsValid}")
     .WithTags("Examples");
 
+// Using type converters this will bind the redirect_uri query string parameter
+// to a Uri object.
+app.MapGet("/redirect", ([FromQuery(Name = "redirect_uri")] TypeConverter<Uri> uri) =>
+    $"The redirect uri = {uri}")
+    .WithTags("Examples");
+
 // Overriding/mutating response defaults using middleware
 app.UseMutateResponse();
 
---
+++ src/MinimalApiPlayground/Properties/TypeConverterOfT.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+
+namespace Microsoft.AspNetCore.Http;
+
+/// <summary>
+/// A custom binder that uses <see cref="TypeConverter"/>s to convert from a <see cref="string"/> to the target <typeparamref name="TValue"/>.
+/// </summary>
+/// <typeparam name="TValue">The that providers a TypeConverter implementation</typeparam>
+public readonly struct TypeConverter<TValue>
+{
+    // Cache the type coverter instance for this generic type
+    private static readonly TypeConverter s_converter = TypeDescriptor.GetConverter(typeof(TValue));
+
+    public TValue Value { get; }
+
+    public TypeConverter(TValue value)
+    {
+        Value = value;
+    }
+
+    public static implicit operator TValue(TypeConverter<TValue> value) => value.Value;
+
+    public override string ToString()
+    {
+        return Value!.ToString()!;
+    }
+
+    public static bool TryParse(string s, out TypeConverter<TValue> result)
+    {
+        if (s_converter is null || !s_converter.CanConvertFrom(typeof(string)))
+        {
+            result = default;
+            return false;
+        }
+
+        var value = (TValue?)s_converter.ConvertFromInvariantString(s);
+
+        if (value is null)
+        {
+            result = default;
+            return false;
+        }
+
+        result = new(value);
+        return true;
+    }
+}
